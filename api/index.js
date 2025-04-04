73d6bdb5ee84267eb4f8935db1c67a7d9218cdeb
softwareengineer68 <developerweb127@gmail.com>
2025-04-04 14:57:02 -0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mysql = require('mysql2');
 const bodyParser = require('body-parser');
 const path = require('path');
+require('dotenv').config(); // Load environment variables from .env file
 
 const app = express();
 
